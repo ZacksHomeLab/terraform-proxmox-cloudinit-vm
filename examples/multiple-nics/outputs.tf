24328adca41816f8f7dfac9b2ea0c48813a7ffe2
ZacksHomeLab <zack@zackshomelab.com>
2023-06-19 22:54:23 -0500

    fix: adjusted example outputs.tf to meet new reqs.

--- examples/multiple-nics/outputs.tf
+++ examples/multiple-nics/outputs.tf
@@ -1,24 +1,40 @@
-output "proxmox_multi_nic_vm_id" {
-  description = "The Virtual Machine's Id."
-  value       = module.multi_nic.proxmox_vm_id
+output "disks" {
+  description = "The Disk(s) affiliated with said Virtual Machine."
+  value       = module.multi_nic.disks
 }
 
-output "proxmox_multi_nic_vm_ip" {
+output "ip" {
   description = "The Virtual Machine's IP on the first Network Adapter."
-  value       = module.multi_nic.proxmox_vm_ip
+  value       = module.multi_nic.ip
 }
 
-output "proxmox_multi_nic_vm_name" {
+output "name" {
   description = "The Virtual Machine's name."
-  value       = module.multi_nic.proxmox_vm_name
+  value       = module.multi_nic.name
 }
 
-output "proxmox_multi_nic_vm_template" {
-  description = "The name of the template in which the Virtual Machine was created on."
-  value       = module.multi_nic.proxmox_vm_template
+output "nics" {
+  description = "The Network Adapter(s) affiliated with said Virtual Machine."
+  value       = module.multi_nic.nics
 }
 
-output "proxmox_multi_nic_vm_node" {
+output "node" {
   description = "The Proxmox Node the Virtual Machine was created on."
-  value       = module.multi_nic.proxmox_vm_node
+  value       = module.multi_nic.node
+}
+
+output "ssh" {
+  description = "The Virtual Machine's SSH Settings."
+  value       = module.multi_nic.ssh_settings
+  sensitive   = true
+}
+
+output "template" {
+  description = "The name of the template in which the Virtual Machine was created on."
+  value       = module.multi_nic.template
+}
+
+output "vmid" {
+  description = "The Virtual Machine's Id."
+  value       = module.multi_nic.vmid
 }
